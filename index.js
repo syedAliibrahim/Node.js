d900da868b8a5380fa254e4004cfe9843b164c93
ali <syedaliibrahim50@gmail.com>
2023-01-19 09:08:44 +0500

    File system

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const app= require('./app')
-console.log(app.z())
+// const app= require('./app')
+// console.log(app.z())
 
-const arr=[4,5,8,7,3,3,5,55,27,9,34,4,8,9,,1,2,3,];
-let arrs=arr.filter((i)=>{
-    return i ===3
-    
-})
-console.log(arrs)
+// const arr=[4,5,8,7,3,3,5,55,27,9,34,4,8,9,,1,2,3,];
+// let arrs=arr.filter((i)=>{
+//     return i ===3
+// })
+// console.log(arrs)
+const fs =require('fs');
+// fs.writeFileSync("ggggggg.js","aaaaaaaadddddddddddddddrrrr")
