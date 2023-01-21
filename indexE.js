68e470d200142f0d1a4d5c737d10e9392f0755fd
ali <syedaliibrahim50@gmail.com>
2023-01-21 17:06:11 +0500

    create rest Api basic

--- indexE.js
+++ indexE.js
@@ -11,13 +11,27 @@ const PORT =5500
 //     res.send('hello, thissss is cccccccccccccc')
 // })
 
-app.use(express.json())
-app.get("/user" ,(req, resp)=>{
-    // console.log(req)
-    resp.send("hello worldaaaaa")
+// app.use(express.json())
+// app.get("/user" ,(req, resp)=>{
+//     // console.log(req)
+//     resp.send("hello worldaaaaa")
+// })
+// app.post("/user" ,(req, resp)=>{
+//     console.log(req.body)
+//     resp.send("hello worlporddt")
+// })
+
+
+app.get("/api/user",(req,res)=>{
+    res.send("user get")
 })
-app.post("/user" ,(req, resp)=>{
-    console.log(req.body)
-    resp.send("hello worlporddt")
+app.post("/api/user",(req,res)=>{
+    res.send("user create ")
+})
+app.put("/api/user",(req,res)=>{
+    res.send("user update ")
+})
+app.delete("/api/user",(req,res)=>{
+    res.send("user delete")
 })
 app.listen(5500)
